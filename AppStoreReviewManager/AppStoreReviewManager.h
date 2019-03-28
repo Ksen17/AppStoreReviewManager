f0f27b7c76c152bbf5b05bacb9bfeb6426a7490b
Ilia Stukalov <iStukalov96@yandex.ru>
2019-03-28 12:04:10 +0300

    Initial Commit

---
+++ AppStoreReviewManager/AppStoreReviewManager.h
@@ -0,0 +1,19 @@
+//
+//  AppStoreReviewManager.h
+//  AppStoreReviewManager
+//
+//  Created by Ilia on 28/03/2019.
+//  Copyright © 2019 Ilia Stukalov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AppStoreReviewManager.
+FOUNDATION_EXPORT double AppStoreReviewManagerVersionNumber;
+
+//! Project version string for AppStoreReviewManager.
+FOUNDATION_EXPORT const unsigned char AppStoreReviewManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppStoreReviewManager/PublicHeader.h>
+
+
